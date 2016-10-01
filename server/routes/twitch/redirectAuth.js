778749e0ec80bc5a9f53aab4be726cab022ec7d2
Aaroh <aarohmankad@gmail.com>
2016-10-01 01:35:51 -0700

    listen for help, respond with usage

--- server/routes/twitch/redirectAuth.js
+++ server/routes/twitch/redirectAuth.js
@@ -1,5 +1,22 @@
+var bot = require('node-twitchbot');
+
 module.exports = function(router) {
-  router.post('/redirectAuth', function (req, res) {
-    console.log("Auth has been successfully redirected");
+  router.get('/redirectAuth', function (req, res) {
+    bot.run({
+      username: 'aarohmankad',
+      oauth: 'oauth:m94g18p2rec7i2lboarwc2qwtprgzz',
+      channel: 'aarohmankad',
+    });
+
+    bot.listenFor('!juke help', function(err, chatter) {
+      if (err) {
+        console.log(err);
+      }
+    
+      console.log(chatter);
+      bot.msg("Usage: !juke [artist name] - [song name]");
+    });
+
+    res.send('Successfully redirected. Check your Twitch channel');
   });
 };

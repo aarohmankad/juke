296702ee1081082a085bf166f586da0596af3364
Aaroh <aarohmankad@gmail.com>
2016-10-01 15:26:44 -0700

    add scope for chat_login, remove test endpoint

--- server/routes/twitch/captureAuth.js
+++ server/routes/twitch/captureAuth.js
@@ -8,8 +8,6 @@ module.exports = function(router) {
       channel: 'aarohmankad'
     };
 
-    console.log(params);
-
     bot.run(params);
 
     bot.listenFor('!juke help', function(err, chatter) {
@@ -21,6 +19,20 @@ module.exports = function(router) {
       bot.msg("Usage: !juke [song name] - [artist name]");
     });
 
+    bot.listen('!juke /queue', function(err, chatter) {
+      if (err) {
+        console.log(err);
+      }
+
+      var message = chatter.msg;
+      var songParts = message.substring(13).split(' - ');
+      var songTitle = songParts[0];
+      var songArtist = songParts[1];
+      
+      console.log("Someone wants to play " + songTitle + " by " + songArtist);
+      bot.msg("Queueing " + songTitle + " by " + songArtist);
+    });
+    
     res.send({
       message: 'Successfully redirected. Check your Twitch channel'
     });

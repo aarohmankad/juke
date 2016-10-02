408072ffececdfa320ce02dec24b53e1dd356289
Aaroh <aarohmankad@gmail.com>
2016-10-02 01:17:18 -0700

    confirmation of song added

--- server/routes/twitch/captureAuth.js
+++ server/routes/twitch/captureAuth.js
@@ -18,7 +18,7 @@ module.exports = function(router) {
       }
 
       console.log("Someone typed !juke");
-      bot.msg("Usage: !juke /queue [song name] - [artist name]\n");
+      bot.msg("Usage: !juke /add [song name] - [artist name]\n");
     });
 
     bot.listenFor('!juke help', function(err, chatter) {
@@ -27,19 +27,19 @@ module.exports = function(router) {
       }
 
       console.log("Someone typed !juke help");
-      bot.msg("Usage: !juke /queue [song name] - [artist name]\n");
+      bot.msg("Usage: !juke /add [song name] - [artist name]\n");
     });
 
-    bot.listen('!juke /queue', function(err, chatter) {
+    bot.listen('!juke /add', function(err, chatter) {
       if (err) {
         console.log(err);
 
-        bot.msg("[Juke says:] Song requests must be formatted as follows: !juke /queue [song name] - [artist name]");
+        bot.msg("[Juke says:] Song requests must be formatted as follows: !juke /add [song name] - [artist name]");
       }
 
       console.log(chatter);
       var message = chatter.msg;
-      var songParts = message.substring(13).split(' - ');
+      var songParts = message.substring(11).split(' - ');
       var songTitle = songParts[0].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
       var songArtist = songParts[1].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 
@@ -48,6 +48,7 @@ module.exports = function(router) {
         .then(function(data) {
             var spotifyURI = data.body.tracks.items[0].uri;
             Spotify.addToPlaylist(spotifyURI);
+            bot.msg(songTitle + ' by ' + songArtist + ' was added');
           }, function(err) {
             console.log('Something went wrong in search!', err);
           });
